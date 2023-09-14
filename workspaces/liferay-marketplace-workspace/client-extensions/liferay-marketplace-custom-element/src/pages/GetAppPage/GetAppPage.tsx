a5523dc0ddbcffca855a2b25bc5b6a87c677d501
Bruno Santana <bruno.santana@liferay.com>
2023-09-14 13:52:49 -0300

    LPS-195699 create the client extension base and the steps

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/GetAppPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/GetAppPage.tsx
@@ -4,19 +4,90 @@
  */
 
 import ClayButton from '@clayui/button';
-import {useState} from 'react';
+import { useState } from 'react';
 
-import {GetAppModal} from '../../components/GetAppModal/GetAppModal';
+import { getSiteURL } from '../../components/InviteMemberModal/services';
+import { Liferay } from '../../liferay/liferay';
+import { StepType } from './enums/stepType';
 
-export default function GetAppPage() {
-	const [showModal, setShowModal] = useState(false);
+type StepComponent = {
+  [key in StepType]?: JSX.Element;
+};
+const sectionProperties = {
+  [StepType.ACCOUNT]: {
+    backStep: StepType.ACCOUNT,
+    nextStep: StepType.LICENSES,
+    title: 'Account Selection',
+  },
+  [StepType.LICENSES]: {
+    backStep: StepType.ACCOUNT,
+    nextStep: StepType.PAYMENT,
+    title: 'License Selection',
+  },
+  [StepType.PAYMENT]: {
+    backStep: StepType.LICENSES,
+    nextStep: StepType.PAYMENT,
+    title: 'Payment Method',
+  },
+};
 
-	return (
-		<>
-			<ClayButton onClick={() => setShowModal(true)}>Get App</ClayButton>
-			{showModal && (
-				<GetAppModal handleClose={() => setShowModal(false)} />
-			)}
-		</>
-	);
-}
+const GetAPPFlow = () => {
+  const [step, setStep] = useState<StepType>(StepType.ACCOUNT);
+
+  const onCancel = () => {
+    Liferay.Util.navigate(getSiteURL());
+  };
+
+  const onContinue = async (nextStep: StepType) => {
+    setStep(nextStep);
+
+    return;
+  };
+
+  const onPrevious = async (previousStep: StepType) => {
+    setStep(previousStep);
+
+    return;
+  };
+
+  const StepFormComponent: StepComponent = {
+    [StepType.ACCOUNT]: <h1>aaa</h1>,
+    [StepType.LICENSES]: <h1>AAAAAAAAA</h1>,
+  };
+
+  return (
+    <div className="border d-flex justify-content-center p-5 rounded">
+      <div className="">
+        <div className="h1 mb-6">{sectionProperties[step].title}</div>
+        <div>{StepFormComponent[step]}</div>
+        <div className="d-flex justify-content-between mt-5">
+          <ClayButton displayType="unstyled" onClick={() => onCancel()}>
+            Cancel
+          </ClayButton>
+          <div>
+            {sectionProperties[step].backStep !== step && (
+              <ClayButton
+                displayType="secondary"
+                onClick={() => onPrevious(sectionProperties[step].backStep)}
+              >
+                Back
+              </ClayButton>
+            )}
+            {sectionProperties[step].nextStep && (
+              <ClayButton
+                className="ml-5"
+                onClick={() => {
+                  onContinue(sectionProperties[step].nextStep);
+                }}
+              >
+                Continue
+              </ClayButton>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetAPPFlow;
